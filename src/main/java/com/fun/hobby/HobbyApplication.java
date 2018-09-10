453d8c0b950f19dcc7e78dc911e0ada4f1ad15cf
pawemy <pawelmyszak92@gmail.com>
2018-09-10 22:23:49 +0200

    first commit

---
+++ src/main/java/com/fun/hobby/HobbyApplication.java
@@ -0,0 +1,12 @@
+package com.fun.hobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbyApplication.class, args);
+    }
+}
